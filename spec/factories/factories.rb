22ee8ebc35e6a0024dabb23b1cab5f12f2a13ba4
Robert Fletcher <lobatifricha@gmail.com>
2012-09-22 16:56:51 -0700

    password authentication tests

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    username {   }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.free_email }
+    password 'pizza'
+    password_confirmation 'pizza'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,25 @@
 require 'spec_helper'
 
 describe User do
-	describe '.authenticate' do
-		context 'given invalid password' do
-			it 'returns false' do
-			end
-		end
+  let(:user) { FactoryGirl.create(:user) }
 
-		context 'given invalid email' do
-			it 'returns false' do
+  describe '.authenticate' do
+    context 'given invalid password' do
+      it 'returns false' do
+        User.authenticate(user.email, 'bad_password').should be_false
+      end
+    end
 
-			end
-		end
+    context 'given invalid email' do
+      it 'returns false' do
+        User.authenticate('bad_email', user.password).should be_false
+      end
+    end
 
-		context 'given valid email and password' do
-			it 'returns the appropriate instance of user' do
-
-			end
-		end
-	end
+    context 'given valid email and password' do
+      it 'returns the appropriate instance of user' do
+        User.authenticate(user.email, user.password).should == user
+      end
+    end
+  end
 end
