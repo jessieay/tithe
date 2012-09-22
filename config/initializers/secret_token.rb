16b2363df314f93524512950aef7befc6d71673c
Jessie A. Young <jessieayoung@Jessie-For-President.local>
2012-09-22 09:22:03 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tithe::Application.config.secret_token = 'a3fd1e17f060fc208740095d4bc34251b9dcee971c7496f99427024247dfa23af01a9bbb81572614f2052c945b916daa168c99885252dd1740dd334eedb9290c'
