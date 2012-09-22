bed872d4c5051a83ed00576bec5b3306adac7a3c
Jessie A. Young <jessie.a.young@gmail.com>
2012-09-22 16:33:27 -0700

    added update and delete to controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -19,6 +19,19 @@ class OrganizationsController < ApplicationController
     @organization = Organization.find(params[:id])
   end
 
+  def update
+    @organization = Organization.find(params[:id])
+      if @organization.update_attributes(params[:organization])
+        redirect_to @organization, notice: 'Organization was successfully updated.'
+      end
+  end
+
+  def destroy
+     @organization = Organization.find(params[:id])
+     @organization.destroy
+     redirect_to organizations_path, notice: 'Organization was successfully deleted.'
+  end
+
   def index
     @organizations = Organization.all
   end
