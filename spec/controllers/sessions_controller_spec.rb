1f52a93bd0c5af462432d212d7d7b6591284cd20
Robert Fletcher <lobatifricha@gmail.com>
2012-09-22 20:29:06 -0700

    adding specs for users_controller and sessions_controller

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+describe SessionsController do
+  render_views
+
+  before :each do
+    @user = FactoryGirl.create(:user)
+  end
+
+  describe '#create' do
+    context 'given valid credentials' do
+      before :each do
+        post(:create, :email => @user.email, :password => @user.password)
+      end
+
+      it 'sets the session user_id' do
+        session[:user_id].should == @user.id
+      end
+
+      it 'flashes a success message' do
+        flash[:notice].should == 'Logged in!'
+      end
+
+      it 'redirects to root_path' do
+        response.should redirect_to(root_path)
+      end
+    end
+
+    context 'given invalid credentials' do
+      before :each do
+        post(:create, :email => @user.email, :password => 'wrongpassword')
+      end
+
+      it 'sets the session user_id to nil' do
+        session[:user_id].should be_nil
+      end
+
+      it 'flashes an error message' do
+        flash.now[:error].should == 'Invalid email or password'
+      end
+
+      it 'renders the new template' do
+        response.should render_template('sessions/new')
+      end
+    end
+  end
+
+  describe '#destroy' do
+    before :each do
+      session[:user_id] = @user.id
+      delete(:destroy)
+    end
+
+    it 'sets the session user_id to nil' do
+      session[:user_id].should be_nil
+    end
+
+    it 'flashes a logged out message' do
+      flash[:notice].should == 'Logged out!'
+    end
+
+    it 'redirects to the root url' do
+      response.should redirect_to(root_url)
+    end
+  end
+end
